3d9cde6d7f753065f066e7f4e3c5e5f7d248a21b
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-05-02 17:29:49 -0300

    Subindo o projeto no GitHub

---
+++ src/main/java/br/edu/unoesc/Desafio20252Application.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio20252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio20252Application.class, args);
+	}
+
+}
