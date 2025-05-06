06a79b4fe6c183d5895db535bf335b09f5c7e653
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-05-06 12:53:48 -0300

    Ajustes no FilmeController para preparar o CRUD

--- src/main/java/br/edu/unoesc/controller/FilmeController.java
+++ src/main/java/br/edu/unoesc/controller/FilmeController.java
@@ -1,5 +1,92 @@
 package br.edu.unoesc.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.edu.unoesc.DTO.FilmeDTO;
+import br.edu.unoesc.model.Filme;
+import br.edu.unoesc.service.FilmeService;
+import br.edu.unoesc.service.TmdbService;
+
+@Controller
+@RequestMapping("/filmes")
 public class FilmeController {
 
+	@Autowired
+	private FilmeService filmeService;
+	
+	@Autowired
+	private TmdbService tmdbService;
+	
+	@GetMapping("/listar")
+	public String listarFilmes(FilmeDTO filmeDTO, Model model) {
+		model.addAttribute("filmeDTO", filmeService.listarFilmesAtivos());
+		return "filmes/listar";
+		
+	}
+	
+	@GetMapping("/cadastrar")
+	public String cadastrarFilme(FilmeDTO filmeDTO, Model model) {
+		model.addAttribute("filmeDTO", filmeDTO);
+		return "paginas/cadastro/cadastrarFilme";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvarFilme(FilmeDTO filmeDTO, Model model, RedirectAttributes attr) {
+		try {
+			filmeService.adicionarFilme(new Filme(filmeDTO));
+			attr.addFlashAttribute("success", "Filme cadastrado com sucesso!");
+			return "redirect:/filmes/listar";
+		} catch (Exception e) {
+			attr.addFlashAttribute("error", "Erro ao cadastrar filme!");
+			return "redirect:/filmes/cadastrar";
+		}
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editarFilme(@PathVariable Integer id, Model model) {
+		Filme filme = filmeService.buscarPorId(id);
+		model.addAttribute("filme", filme);
+		return "paginas/editar/editarFilme";
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public String deletarFilme(@PathVariable Integer id) {
+		filmeService.deletarFilme(id);
+		return "redirect:/filmes/listar";
+	}
+
+	@GetMapping("/buscar-filmes")
+	@ResponseBody
+	public ResponseEntity<?> buscarFilmes(@RequestParam String titulo) {
+	    try {
+	        List<FilmeDTO> filmes = tmdbService.buscarFilmesPorTitulo(titulo);
+	        return ResponseEntity.ok(filmes);
+	    } catch (Exception e) {
+	        return ResponseEntity.badRequest().body("Erro ao buscar filmes");
+	    }
+	}
+
+	@GetMapping("/filme-tmdb")
+	@ResponseBody
+	public ResponseEntity<FilmeDTO> obterDetalhesFilme(@RequestParam Integer id) {
+	    try {
+	        FilmeDTO filme = tmdbService.buscarFilmePorId(id);
+	        return ResponseEntity.ok(filme);
+	    } catch (Exception e) {
+	        return ResponseEntity.badRequest().body(null);
+	    }
+	}
+	
 }
