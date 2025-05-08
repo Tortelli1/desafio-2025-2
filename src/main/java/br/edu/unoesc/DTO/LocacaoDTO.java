39e3eb11714adf2084f1fbba9c9196ba8028a582
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-05-08 18:23:51 -0300

    Configuração dos DTO's de locação

--- src/main/java/br/edu/unoesc/DTO/LocacaoDTO.java
+++ src/main/java/br/edu/unoesc/DTO/LocacaoDTO.java
@@ -2,8 +2,34 @@ package br.edu.unoesc.DTO;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record LocacaoDTO(List<FilmeDTO> listaFilmes, String nome, String cpf, String email, String telefone, Date dataLocacao, Date dataDevolucao, Date dataDevolvido) {
+import br.edu.unoesc.model.Locacao;
+
+public record LocacaoDTO(
+		List<ExemplarDTO> listaExemplares, 
+	    String nome, 
+	    String cpf, 
+	    String email, 
+	    String telefone, 
+	    Date dataLocacao, 
+	    Date dataDevolucao, 
+	    Date dataDevolvido
+		) {
 	
+	public LocacaoDTO(Locacao locacao) {
+        this(
+            locacao.getExemplares().stream()
+                .map(ExemplarDTO::configuraExemplar)
+                .collect(Collectors.toList()), 
+            locacao.getNome(),
+            locacao.getCpf(),
+            locacao.getEmail(),
+            locacao.getTelefone(),
+            locacao.getDataLocacao(),
+            locacao.getDataDevolucao(),
+            locacao.getDataDevolvido()
+        );
+    }
 
 }
--- src/main/java/br/edu/unoesc/DTO/LocacaoQRCodeDTO.java
+++ src/main/java/br/edu/unoesc/DTO/LocacaoQRCodeDTO.java
@@ -3,6 +3,18 @@ package br.edu.unoesc.DTO;
 import java.util.Date;
 import java.util.List;
 
-public record LocacaoQRCodeDTO(Integer id, String nome, String cpf, String email, String telefone, Date dataLocacao, Date dataDevolucao, Date dataDevolvido, String qrCodeBase64, List<FilmeDTO> listaFilmes) {
+public record LocacaoQRCodeDTO(
+	    Integer id, 
+	    String nome, 
+	    String cpf, 
+	    String email, 
+	    String telefone, 
+	    Date dataLocacao, 
+	    Date dataDevolucao, 
+	    Date dataDevolvido, 
+	    String qrCodeBase64, 
+	    List<FilmeDTO> listaFilmes,
+	    Boolean locacaoDevolvida
+	) {
 
 }
