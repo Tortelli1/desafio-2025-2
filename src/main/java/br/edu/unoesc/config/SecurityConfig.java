d6186cfd2a3c6e7dfa2e195a8e6b9e33f3ecb2a8
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-05-13 17:52:18 -0300

    Configuração do Spring Security

---
+++ src/main/java/br/edu/unoesc/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package br.edu.unoesc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityChain(HttpSecurity http) throws Exception {
+		
+		http
+			.csrf(csrf -> csrf.disable())
+			
+			.authorizeHttpRequests(requests -> requests
+                        .requestMatchers(
+                        		"/", 
+                        		"/css/paginas/login.css", 
+                        		"/css/",
+                        		"/consultaPublica/consultarLocacao").permitAll()
+                        .anyRequest().authenticated())
+			
+			.formLogin(login -> login
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/home", true)
+                                .permitAll())
+			.logout(logout -> logout
+					.logoutUrl("/logout")
+					.logoutSuccessUrl("/login")
+					.permitAll());
+	
+		return http.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("marco")
+				.password(passwordEncoder().encode("unoesc@2024"))
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
