59a99a6f4f4fbaec368c5d130f37720acfc04afa
Marco Tortelli <marcopazinitortelli@gmail.com>
2025-05-14 11:47:32 -0300

    Ajustado para redirecionar para a tela de login no primeiro acesso

--- src/main/java/br/edu/unoesc/config/SecurityConfig.java
+++ src/main/java/br/edu/unoesc/config/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig {
 			
 			.authorizeHttpRequests(requests -> requests
                         .requestMatchers(
-                        		"/", 
                         		"/css/**",
                         		"/js/**",
                         		"/consultaPublica/**",
